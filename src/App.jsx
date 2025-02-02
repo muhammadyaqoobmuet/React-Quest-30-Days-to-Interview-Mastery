97eb4c9321c6cb23bf7a625e7f4039e31229ca6d
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-02-02 23:05:57 +0500

    problem day 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import PhoneOtpForm from "./pages/Otp-Login/PhoneOtpForm"
 
 
 function App() {
-  
+
 
   return (
-    <>
-    <h1 className="">Hello World</h1>
-    </>
-  ) 
+    <div className="bg-[#0d0d0d] h-screen w-full">
+      {/* day one */}
+      <PhoneOtpForm />
+    </div>
+  )
 }
 
 export default App
