83f046f8bcf895f00859a08f7e26f886976e7656
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-02-03 22:48:00 +0500

    day two question - progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react"
 import PhoneOtpForm from "./pages/Otp-Login/PhoneOtpForm"
+import Progress from "./pages/Progress-Bar/Progress"
 
 
 function App() {
+  //for day 2 question
+  const [value, setValue] = useState(0)
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((value) => value + 1)
 
+    }, 100)
+
+    //remove
+    if (value >= 100) {
+      clearInterval(interval)
+    }
+    return () => {
+      clearInterval(interval)
+    }
+
+  }, [])
   return (
     <div className="bg-[#0d0d0d] h-screen w-full">
       {/* day one */}
-      <PhoneOtpForm />
+      {/* <PhoneOtpForm /> */}
+      {/* day 2 */}
+      <Progress value={value} />
     </div>
   )
 }
